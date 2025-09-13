5bb943c0fc6416157e64d344e2094c78bd274f21
nhatvydayyy <nhatvykimdong@gmail.com>
2025-09-14 00:28:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function orderNow() {
+  alert("📦 Cảm ơn bạn đã chọn Fresh Orange Juice! Chúng tôi rất nóng lòng để được phục vụ bạn.");
+}
+
+function seeMenu() {
+  alert("🍹 Menu đang được cập nhật. Vui lòng quay lại sau! SEE YOU SOON!");
+}
